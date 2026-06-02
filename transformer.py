a7cf7fee757086ab81729e466ceb753388051a2e
daniel <18262962+danieldonatsch@users.noreply.github.com>
2026-06-02 09:32:11 +0200

    Transformer can now handle multiple attention heads and several layers.

--- transformer.py
+++ transformer.py
@@ -8,25 +8,38 @@ from multilayer_perceptron import MultilayerPerceptron
 
 class Transformer(nn.Module):
 
-    def __init__(self, d_embedding: int, d_key_query_space: int, device=torch.device('cpu')):
+    def __init__(self, d_embedding: int, d_key_query_space: int, device=torch.device('cpu'),
+                 num_layers=2, num_attention_heads=3):
         super().__init__()
         self.device = device
 
-        self.self_attention = Attention(d_embedding=d_embedding, d_key=d_key_query_space).to(self.device)
-        # Add an MLP Layer
-        self.multilayer_perceptron = MultilayerPerceptron(d_embedding=d_embedding).to(self.device)
+        self.attention_blocks = []
+        self.multilayer_perceptrons = []
+
+        for layer in range(num_layers):
+            # Create Attention Blocks
+            self.attention_blocks.append(
+                [Attention(d_embedding=d_embedding, d_key=d_key_query_space).to(self.device)
+                 for _ in range(num_attention_heads)]
+            )
+            # Create MLP blocks
+            self.multilayer_perceptrons.append(
+                MultilayerPerceptron(d_embedding=d_embedding).to(self.device)
+            )
 
     def forward(self, embeddings):
 
         mask = torch.tril(torch.ones((embeddings.size(dim=0), embeddings.size(dim=0)), device=self.device))
         mask = mask == 0
 
-        # Compute Self Attention
-        self_attention_values = self.self_attention(embeddings, embeddings, embeddings, mask)
+        for i in range(len(self.attention_blocks)):
+            # Apply all attention blocks of the layer
+            self_attention_values = torch.zeros(embeddings.size(), device=self.device)
+            for attention_block in self.attention_blocks[i]:
+                self_attention_values += attention_block(embeddings, embeddings, embeddings, mask)
+            # Update the embeddings with the self attention values
+            embeddings = embeddings + self_attention_values
+            # Apply the MLP
+            embeddings = self.multilayer_perceptrons[i](embeddings)
 
-        # Add self attention to the embeddings
-        transformed_values = embeddings + self_attention_values
-
-        transformed_values = self.multilayer_perceptron(transformed_values)
-
-        return transformed_values
+        return embeddings
